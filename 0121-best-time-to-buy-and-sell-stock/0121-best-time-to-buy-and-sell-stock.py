a4285cfe203b8a4ec4569e32e8e287c232a699a1
Adrien LUDWIG <42720099+Adrien-LUDWIG@users.noreply.github.com>
2023-10-18 18:19:28 +0200

    Time: 8243 ms (5%), Space: 22.5 MB (60.5%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -1,9 +1,18 @@
-def last_max_index(array, value):
-    array.reverse()
-    index = array.index(value)
-    array.reverse()
-    return len(array) - index - 1 
+def max_with_index(array, slice_offset=0):
+    array = array[slice_offset:]
 
+    max_value = array[-1]
+    max_index = len(array) - 1
+
+    index = len(array) - 1
+    while index >= 0:
+        if array[index] > max_value:
+            max_value = array[index]
+            max_index = index
+        index -= 1
+    
+    return max_value, max_index + slice_offset
+    
 
 class Solution(object):
     def maxProfit(self, prices):
@@ -12,23 +21,23 @@ class Solution(object):
         :rtype: int
         """
 
-        max_sell_index = 0
-        max_profit = 0
+        if len(prices) < 2:
+            return 0
 
-        while max_sell_index < len(prices) - 1:
-            max_sell_price = max(prices[max_sell_index+1:])
+        max_sell_price, max_sell_index = max_with_index(prices, 1)
+        min_buy_price = min(prices[:max_sell_index])
+        max_profit = max(0, max_sell_price - min_buy_price)
+
+        while max_sell_index < len(prices) - 2:
+            max_sell_price, new_max_sell_index = max_with_index(prices, max_sell_index+2)
 
             if max_sell_price <= max_profit:
                 break
 
-            new_max_sell_index = last_max_index(prices, max_sell_price)
+            print(max_sell_index, new_max_sell_index)
+            min_buy_price = min(prices[max_sell_index+1:new_max_sell_index])
 
-            min_buy_price = min(prices[max_sell_index:new_max_sell_index])
-
-            new_max_profit = max_sell_price - min_buy_price
-
-            if new_max_profit > max_profit:
-                max_profit = new_max_profit
+            max_profit = max(max_profit, max_sell_price - min_buy_price)
 
             max_sell_index = new_max_sell_index 
 
