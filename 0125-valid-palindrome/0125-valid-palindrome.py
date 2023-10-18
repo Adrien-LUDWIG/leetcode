4987ecf6b49667d2b274d7d0520d82ffdcb0c7b2
Adrien LUDWIG <42720099+Adrien-LUDWIG@users.noreply.github.com>
2023-10-18 19:11:41 +0200

    Time: 28 ms (69.93%), Space: 14 MB (83.3%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -4,7 +4,27 @@ class Solution(object):
         :type s: str
         :rtype: bool
         """
+
+        # Version 1: filter, lower, reverse
         # Keep lower alphanum characters
-        clean_string = "".join([character.lower() for character in s if character.isalnum()])
+        # clean_string = "".join([character.lower() for character in s if character.isalnum()])
         # Compare to reversed string
-        return clean_string == clean_string[::-1]
+        # return clean_string == clean_string[::-1]
+
+        # Version 2: two pointers
+
+        start = 0
+        end = len(s) - 1
+
+        while start < end:
+            while not s[start].isalnum() and start < end:
+                start += 1
+            while not s[end].isalnum() and start < end:
+                end -= 1
+            if s[start].lower() == s[end].lower():
+                start += 1
+                end -= 1
+            else:
+                return False
+
+        return True 
