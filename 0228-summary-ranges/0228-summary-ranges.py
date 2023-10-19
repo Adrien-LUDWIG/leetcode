146a7bc24d67bdeaea5b731cbfc293019ec996e9
Adrien LUDWIG <42720099+Adrien-LUDWIG@users.noreply.github.com>
2023-10-19 11:35:18 +0200

    Time: 14 ms (49.82%), Space: 13.2 MB (88.19%) - LeetHub

--- 0228-summary-ranges/0228-summary-ranges.py
+++ 0228-summary-ranges/0228-summary-ranges.py
@@ -5,29 +5,21 @@ class Solution(object):
         :rtype: List[str]
         """
 
+        # If the array is empty, there is no range
         if len(nums) == 0:
             return []
         
         ranges = []
-
         range_start = 0
 
-        for range_end in range(len(nums) - 1):
-            # If the next number directly follows this number, continue
-            if nums[range_end + 1] - nums[range_end] == 1:
-                continue
-                    
-            # Else, append the appropriate range representation
-            if range_start == range_end:
-                ranges.append(str(nums[range_start]))
-            else:
-                ranges.append(str(nums[range_start]) + "->" + str(nums[range_end]))
-            range_start = range_end + 1
-
-        # Add last range
-        if range_start == (len(nums) - 1):
-            ranges.append(str(nums[-1]))
-        else:
-            ranges.append(str(nums[range_start]) + "->" + str(nums[-1]))
+        for range_end in range(len(nums)):
+            # If we're at the end of the list or if numbers aren't consecutive
+            if range_end == len(nums) - 1 or nums[range_end + 1] - nums[range_end] != 1:
+                # Add a new range to the list
+                if range_start == range_end:              # Only one number
+                    ranges.append(str(nums[range_start]))
+                else:                                     # Multiple numbers
+                    ranges.append(str(nums[range_start]) + "->" + str(nums[range_end]))
+                range_start = range_end + 1
 
         return ranges
