9b3cef1a0d2598f15a913fadea8ce784c2906847
Adrien LUDWIG <42720099+Adrien-LUDWIG@users.noreply.github.com>
2023-10-22 02:22:04 +0200

    Time: 1237 ms (90%), Space: 24.9 MB (80%) - LeetHub

--- 1286-constrained-subsequence-sum/1286-constrained-subsequence-sum.py
+++ 1286-constrained-subsequence-sum/1286-constrained-subsequence-sum.py
@@ -9,7 +9,11 @@ class Solution(object):
         """
 
         sub_sums = [nums[0]]
+
+        # Max of sub_sums
         max_sub_sum = sub_sums[0]
+
+        # Max of the last k sub_sums
         sliding_max = deque()
         sliding_max.append(sub_sums[0])
 
@@ -18,11 +22,16 @@ class Solution(object):
             new_sub_sum = nums[i] + max(0, sliding_max[0])
             sub_sums.append(new_sub_sum)
 
-            if i >= k and sliding_max and sliding_max[0] == sub_sums[i - k]:
-                sliding_max.popleft()
+            # Update sliding window max
+            # Remove too small values
             while sliding_max and sliding_max[-1] < new_sub_sum:
                 sliding_max.pop()
+            # Append new value
             sliding_max.append(new_sub_sum)
+            # Remove sliding window leftmost value
+            # (which will leave the sliding window in next loop)
+            if i >= k and sliding_max[0] == sub_sums[i - k]:
+                sliding_max.popleft()
 
             max_sub_sum = max(max_sub_sum, new_sub_sum)
 
