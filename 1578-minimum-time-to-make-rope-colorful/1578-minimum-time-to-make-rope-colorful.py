d173501df6c388ab6c45b13bd4283679ebdb1481
Adrien LUDWIG <42720099+Adrien-LUDWIG@users.noreply.github.com>
2023-12-30 00:26:15 +0100

    Time: 1666 ms (9.39%), Space: 28.3 MB (29.96%) - LeetHub

--- 1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.py
+++ 1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.py
@@ -1,20 +1,5 @@
+from itertools import groupby
+
 class Solution:
     def minCost(self, colors: str, neededTime: List[int]) -> int:
-        
-        total_time = 0
-        
-        last_color = None
-        max_time = 0
-        sum_time = 0
-        
-        for color, time in zip(colors, neededTime):
-            if color != last_color:
-                total_time += sum_time - max_time
-                last_color = color
-                max_time = time
-                sum_time = time
-            else: # color == last_color
-                max_time = max(time, max_time)
-                sum_time += time 
-        total_time += sum_time - max_time
-        return total_time
+        return sum(sum(grp1) - max(grp2) for grp1, grp2 in (tee(grp) for _, grp in groupby(neededTime, key=lambda _, iter_colors=iter(colors): next(iter_colors))))
