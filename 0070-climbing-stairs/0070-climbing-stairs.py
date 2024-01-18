073a070b585d4f2df8981b34bcf944bae6852d0f
Adrien LUDWIG <42720099+Adrien-LUDWIG@users.noreply.github.com>
2024-01-18 14:53:08 +0100

    Time: 44 ms (19.73%), Space: 17.2 MB (31.72%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,10 +1,13 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        odd = 1
-        even = 1
+        # Possibilities for 0 and 1 stairs
+        even = 1 # 0 stairs
+        odd = 1 # 1 stairs
 
+        # Climb stairs 2 by 2, counting possibilities
         for _ in range(n // 2):
-            odd += even
-            even += odd
+            even += odd # i * 2 stairs
+            odd += even # i * 2 + 1 stairs
 
-        return even if n % 2 else odd
+        # Return the number of possibilities for n stairs
+        return odd if n % 2 else even
