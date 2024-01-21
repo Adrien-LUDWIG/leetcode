6c8325e7ffad66c939f5b70baeb9169e7e82108c
Adrien LUDWIG <42720099+Adrien-LUDWIG@users.noreply.github.com>
2024-01-21 16:52:46 +0100

    Time: 620 ms (31.38%), Space: 28.8 MB (97.01%) - LeetHub

--- 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.py
+++ 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.py
@@ -4,13 +4,19 @@ class Solution:
 
         # Sort jobs by end time
         jobs = sorted(zip(endTime, startTime, profit))
-        endTime = [job[0] for job in jobs]
 
         # Find the best possible profit
-        dp = [0] * (n + 1)
+        dp = [0]
+        ends = [0]
 
         for i, (end, start, profit) in enumerate(jobs, start=1):
-            last_compatible = bisect_right(endTime, start, hi=i)
-            dp[i] = max(profit + dp[last_compatible], dp[i - 1])
+            # Find last compatible job to test if the current one should be kept
+            last_compatible = bisect_right(ends, start) - 1
+            total_profit = max(profit + dp[last_compatible], dp[- 1])
+            
+            # If keeping this job is the new best choice, add it to the possibilities
+            if total_profit > dp[-1]:
+                dp.append(total_profit)
+                ends.append(end)
         
         return dp[-1]
