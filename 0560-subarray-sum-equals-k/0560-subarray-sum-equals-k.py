2afe239b7e303a67c823f1e84d0b4b6999a03a03
Adrien LUDWIG <42720099+Adrien-LUDWIG@users.noreply.github.com>
2024-01-28 19:00:09 +0100

    Time: 232 ms (71.44%), Space: 19.5 MB (70.58%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
@@ -1,7 +1,6 @@
 class Solution:
     def subarraySum(self, nums: List[int], target: int) -> int:
-        sums = defaultdict(int)
-        sums[0] = 1 # The empty array
+        sums = {0: 1} # The empty array
         prefix_sum = 0
         count = 0
 
@@ -9,8 +8,10 @@ class Solution:
             prefix_sum += nums[i]
             complementary = prefix_sum - target
 
-            count += sums[complementary]
-            sums[prefix_sum] += 1
+            if complementary in sums:
+                count += sums[complementary]
+
+            sums[prefix_sum] = sums.get(prefix_sum, 0) + 1
 
         return count
         
